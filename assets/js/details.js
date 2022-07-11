e6ff79c19dcd1a20d46492f6e44ebc75352f71d3
yathavanyogarajah <yathavancom@gmail.com>
2022-07-11 15:02:20 +0530

    loan section added

--- assets/js/details.js
+++ assets/js/details.js
@@ -13,4 +13,24 @@ $(document).ready(function () {
         $('.features-container').toggleClass('show-less');
     });
 
+    new Swiper('.payment-plan-swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.payment-next',
+            prevEl: '.payment-prev',
+        },
+        slidesPerView: 4,
+        spaceBetween: 10,
+    });
+
+    new Swiper('.loan-swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.loan-next',
+            prevEl: '.loan-prev',
+        },
+        slidesPerView: 4,
+        spaceBetween: 10,
+    });
+
 });
