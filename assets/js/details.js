42aab37f902f2480a81422aa74c6193796cf5c3e
yathavanyogarajah <yathavancom@gmail.com>
2022-07-18 02:07:37 +0530

    all done - 1

--- assets/js/details.js
+++ assets/js/details.js
@@ -23,49 +23,136 @@ $(document).ready(function () {
     });
 
     new Swiper('.payment-plan-swiper', {
-        loop: true,
+        loop: false,
         navigation: {
             nextEl: '.payment-next',
             prevEl: '.payment-prev',
         },
         slidesPerView: 4,
         spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 3.5,
+            },
+            576: {
+                slidesPerView: 4.5,
+            },
+            768: {
+                slidesPerView: 5,
+            },
+            992: {
+                slidesPerView: 5,
+            }
+        }
     });
 
     new Swiper('.loan-swiper', {
-        loop: true,
+        loop: false,
         navigation: {
             nextEl: '.loan-next',
             prevEl: '.loan-prev',
         },
         slidesPerView: 4,
         spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2.5,
+            },
+            576: {
+                slidesPerView: 3.5,
+            },
+            768: {
+                slidesPerView: 4.5,
+            },
+            992: {
+                slidesPerView: 4.5,
+            }
+        }
+    });
+
+    new Swiper('.videos-menu-swiper', {
+        loop: false,
+        slidesPerView: "auto",
+        spaceBetween: 10,
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //         slidesPerView: 1,
+        //     },
+        //     576: {
+        //         slidesPerView: 1,
+        //     },
+        //     768: {
+        //         slidesPerView: 2,
+        //     },
+        //     992: {
+        //         slidesPerView: 3,
+        //     }
+        // }
     });
 
     new Swiper('.other-project-swiper', {
-        loop: true,
+        loop: false,
         navigation: {
-            nextEl: '.loan-next',
-            prevEl: '.loan-prev',
+            nextEl: '.other-project-next',
+            prevEl: '.other-project-prev',
         },
         slidesPerView: 3,
         spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            }
+        }
     });
 
     new Swiper('.mapview-swiper', {
-        loop: true,
-        slidesPerView: "auto",
+        loop: false,
+        slidesPerView: 4.5,
         navigation: {
             nextEl: '.mapView-next',
             prevEl: '.mapView-prev',
         },
         spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 3.5,
+            },
+            576: {
+                slidesPerView: 4.5,
+            },
+            768: {
+                slidesPerView: 5.5,
+            },
+            992: {
+                slidesPerView: 5.5,
+            },
+            1200: {
+                slidesPerView: 6.5,
+            },
+            1440: {
+                slidesPerView: 7.5,
+            }
+        }
     });
 
 
     //property rate chart
 
-    const ctx = document.getElementById('propertyRateChart').getContext('2d');
+    var ctx = document.getElementById('propertyRateChart').getContext('2d');
 
     var gradient = ctx.createLinearGradient(0, 0, 0, 400);
     gradient.addColorStop(0, 'rgba(251,183,87,0.50)');
@@ -95,18 +182,18 @@ $(document).ready(function () {
     });
 
 
-    var swiper = new Swiper(".brochure-swiper", {
-        // slidesPerView: 3,
-        pagination: {
-            el: ".swiper-pagination",
-            type: "progressbar",
-        },
-        loop:false,
-        navigation: {
-            nextEl: ".brochure-next",
-            prevEl: ".brochure-prev",
-        },
-    });
+    // var swiper = new Swiper(".brochure-swiper", {
+    //     // slidesPerView: 3,
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         type: "progressbar",
+    //     },
+    //     loop:false,
+    //     navigation: {
+    //         nextEl: ".brochure-next",
+    //         prevEl: ".brochure-prev",
+    //     },
+    // });
 
     var swiper = new Swiper(".banner-swiper", {
         // slidesPerView: 3,
@@ -133,5 +220,156 @@ $(document).ready(function () {
         initialCountry: "lk",
         allowDropdown:true,
     });
+
+    var initPhoneNo = document.querySelector("#initPhoneNo");
+    window.intlTelInput(initPhoneNo, {
+        initialCountry: "lk",
+        allowDropdown:true,
+    });
+
+    var DetailsPhoneNoModal = new bootstrap.Modal(document.getElementById('DetailsPhoneNoModal'),)
+    var detailsInitPopup = new bootstrap.Modal(document.getElementById('detailsPopup'),);
+
+    $('#btnShowPhone').on("click",function () {
+
+        $('#detailsCallbackBtnClose').trigger("click");
+        setTimeout(()=> DetailsPhoneNoModal.show(),0);
+    });
+
+    setTimeout(()=>{
+        detailsInitPopup.show();
+    },30000);
+
+    $('#showSpecification').on("click",function (){
+        $('#specContent').toggleClass('show-less');
+    });
+
+
+    $('#showTeam').on("click",function (){
+        $('#teamShowMoreContent').toggleClass('show-less');
+    });
+
+    $('#showEnquireNum').on("click",function (){
+       $('.phone-info').removeClass('hide');
+       $(this).hide();
+    });
+
+
+    //pdf broucher
+
+    // If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+//     var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+    var url = './assets/pdf/sample.pdf';
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    var pdfDoc = null,
+        pageNum = 1,
+        pageRendering = false,
+        pageNumPending = null,
+        scale = 1,
+        canvas = document.getElementById('the-canvas'),
+        container = document.getElementById('broucher-pdf-container'),
+        ctx = canvas.getContext('2d');
+
+    /**
+     * Get page info from document, resize canvas accordingly, and render page.
+     * @param num Page number.
+     */
+    function renderPage(num) {
+        pageRendering = true;
+        // Using promise to fetch the page
+        pdfDoc.getPage(num).then(function(page) {
+
+            var viewport = page.getViewport({scale: scale});
+
+            // var scale_ = container.clientWidth / viewport.width;
+
+            // viewport = page.getViewport(scale_);
+
+            // canvas.height = viewport.height;
+
+          const containerWidth = document.getElementById("broucher-pdf-container").offsetWidth;
+
+            canvas.height = 400;
+            canvas.width = containerWidth -32 ;
+            // canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: ctx,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+
+            // Wait for rendering to finish
+            renderTask.promise.then(function() {
+                pageRendering = false;
+                if (pageNumPending !== null) {
+                    // New page rendering is pending
+                    renderPage(pageNumPending);
+                    pageNumPending = null;
+                }
+            });
+        });
+
+        // Update page counters
+        // document.getElementById('page_num').textContent = num;
+        var progressWidth = ((num/pdfDoc.numPages)*100) + '%';
+        $('#brochureProgressBar').width(progressWidth);
+    }
+
+    /**
+     * If another page rendering in progress, waits until the rendering is
+     * finised. Otherwise, executes rendering immediately.
+     */
+    function queueRenderPage(num) {
+        if (pageRendering) {
+            pageNumPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+
+    /**
+     * Displays previous page.
+     */
+    function onPrevPage() {
+        if (pageNum <= 1) {
+            return;
+        }
+        pageNum--;
+        queueRenderPage(pageNum);
+    }
+    document.getElementById('brochurePrev').addEventListener('click', onPrevPage);
+
+    /**
+     * Displays next page.
+     */
+    function onNextPage() {
+        if (pageNum >= pdfDoc.numPages) {
+            return;
+        }
+        pageNum++;
+        queueRenderPage(pageNum);
+    }
+    document.getElementById('brochureNext').addEventListener('click', onNextPage);
+
+    /**
+     * Asynchronously downloads PDF.
+     */
+    pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+        pdfDoc = pdfDoc_;
+        // document.getElementById('page_count').textContent = pdfDoc.numPages;
+
+        // Initial/first page rendering
+        renderPage(pageNum);
+    });
+
 
 });
